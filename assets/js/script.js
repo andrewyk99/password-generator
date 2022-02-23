21a0beed9f5b8d4140bf440b57bea518b887551c
andrewyk99 <andrewyk99@hotmail.com>
2022-02-23 01:00:16 -0700

    created function for asking length of password and set conditions to ask again if invalid answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,32 @@
 // Assignment code here
+var generatePassword = function () {
+  var passwordPrompt = firstQuestion();
+  console.log(passwordPrompt);
+  return passwordPrompt;
+}
+
+// function to ask password length
+var firstQuestion = function () {
+  var passwordLength = window.prompt("How many characters for password? (8-128)");
+  passwordLength = parseInt(passwordLength);
+  if (isNaN (passwordLength) || passwordLength === "" || passwordLength === null || passwordLength < 8 || passwordLength > 128) {
+    window.alert("You need to provide a valid answer!")
+    return firstQuestion();
+  }
+
+  // if (passwordLength === "" || passwordLength === null) {
+    // window.alert("You need to provide a valid answer!");
+    // return firstQuestion();
+  //}
+
+  //if (passwordLength < 8 || passwordLength > 128) {
+    //window.alert("You need to provide a valid answer!");
+    //return firstQuestion();
+  //}
+
+  return passwordLength;
+};
+
 
 
 // Get references to the #generate element
